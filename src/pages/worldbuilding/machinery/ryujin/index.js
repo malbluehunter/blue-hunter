51a3bcc8dbd31ce6e3720d605447ac7c2f9ce283
emi kashizaki@otsukai <kashizaki@otsukai.com>
2021-06-01 15:33:50 +0900

    add: ワールド メカ・アイテムページ追加

---
+++ src/pages/worldbuilding/machinery/ryujin/index.js
@@ -0,0 +1,73 @@
+import React from "react"
+import * as styles from "./index.module.scss"
+import TextHeading from "../../../../components/atoms/TextHeading"
+import TextMedium from "../../../../components/atoms/TextMedium"
+import TextLeading from "../../../../components/atoms/TextLeading"
+import TextAnnotation from "../../../../components/atoms/TextAnnotation"
+import TextMediumSub from "../../../../components/atoms/TextMediumSub"
+import { Helmet } from "react-helmet"
+import LayoutTwoColumn from "../../../../components/templates/LayoutTwoColumn"
+import PageTitle from "../../../../components/atoms/PageTitle"
+import Bread from "../../../../components/molecules/Bread"
+import MainContentsTop from "../../../../components/organisms/MainContentsTop"
+import MainContentsMiddleWorld from "../../../../components/organisms/MainContentsMiddleWorld"
+import ContentsLine from "../../../../components/molecules/ContentsLine"
+import MainContentsBottom from "../../../../components/organisms/MainContentsBottom"
+import WorldContentsInner from "../../../../components/organisms/WorldContentsInner"
+import PagerArea from "../../../../components/organisms/PagerArea"
+
+const WorldPage = () => {
+  return (
+    <>
+      <Helmet>
+        <html lang="ja" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>世界観・設定 | 「BLUE HUNTER」公式サイト</title>
+        <meta name="description" content="ブルーハンタープロジェクト公式サイト。2021年6月より漫画「BLUE HUNTER」を連載開始！最新ニュースや設定資料集、スペシャル情報等をこちらからお届けします。" />
+        <meta name="keywords" content="BLUE HUNTER, ブルーハンター, ブルーハンタープロジェクト, 無料漫画, アートコンペ " />
+        <link rel="canonical" href="https://manga.uminohi.jp/worldbuilding/machinery/ryujin/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/worldbuilding/machinery/ryujin/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="「BLUE HUNTER」公式サイト" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+        <link rel="preload" as="image" href="/common/nav_text_news_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_comic_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_world_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_characters_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_staff_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_special_hover.png" />
+      </Helmet>
+
+      {/* --------------------------------------- mal様修正範囲 開始 --------------------------------------- */}
+      <LayoutTwoColumn>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/world/ttl_world.png" alt="The World of BLUE HUNTER" />
+        </div>
+        {/* パンクズリスト */}
+        <Bread page01="TOP" href01="/" page02="世界観・設定" href02="../../index.html" page03="メカ・アイテム - ウェイクライダー 龍神" />
+        <WorldContentsInner>
+          <MainContentsTop text="メカ・アイテム" />
+          <MainContentsMiddleWorld>
+            <TextHeading text="ウェイクライダー 龍神" />
+            <ContentsLine />
+            <TextMedium text="100km/h以上ものスピードを誇るウェイクライダー「⿓神」（遠隔操縦ジェットスキー&amp;合体分離式ウェイクボード）を操る少年が、⽚⼿にボール型ユーティリティのナノボールを持っている。⽔上ミッションではウェイクライダーの⾳で⽣物をドライブ（追う）またはエスケイプ（逃げる）しつつ、ハンターは最⼤100メートルもの距離をとってターゲットの前後へ、時には空中をジャンプしつつ⾃在に回り込む。⽔中ミッションではシーポッド（いわゆる⽔中スクーター）がこの役割を担う。" />
+            <div className={styles.img_wrapper}>
+              <img src="/world/world_img_machinery_03.jpg" alt="" />
+            </div>
+            <div className={styles.img_wrapper}>
+              <img src="/world/world_img_machinery_04.jpg" alt="" />
+            </div>
+          </MainContentsMiddleWorld>
+          <MainContentsBottom />
+          {/* PREV、NEXTボタンのURL、ページ番号の設定。1ページ目はPREVボタンを非活性、最終ページはNEXTボタンを非活性にします。 各ページごとに設定お願いします。*/}
+          <PagerArea hrefPrev="../orca/" hrefNext="../bounty-kit/" isDisabledPrev={false} isDisabledNext={false}  number={2} />
+      {/* --------------------------------------- mal様修正範囲 終了 --------------------------------------- */}
+        </WorldContentsInner>
+      </LayoutTwoColumn>
+    </>
+  )
+}
+
+export default WorldPage
---
+++ src/pages/worldbuilding/machinery/sigma-glasses/index.js
@@ -0,0 +1,70 @@
+import React from "react"
+import * as styles from "./index.module.scss"
+import TextHeading from "../../../../components/atoms/TextHeading"
+import TextMedium from "../../../../components/atoms/TextMedium"
+import TextLeading from "../../../../components/atoms/TextLeading"
+import TextAnnotation from "../../../../components/atoms/TextAnnotation"
+import TextMediumSub from "../../../../components/atoms/TextMediumSub"
+import { Helmet } from "react-helmet"
+import LayoutTwoColumn from "../../../../components/templates/LayoutTwoColumn"
+import PageTitle from "../../../../components/atoms/PageTitle"
+import Bread from "../../../../components/molecules/Bread"
+import MainContentsTop from "../../../../components/organisms/MainContentsTop"
+import MainContentsMiddleWorld from "../../../../components/organisms/MainContentsMiddleWorld"
+import ContentsLine from "../../../../components/molecules/ContentsLine"
+import MainContentsBottom from "../../../../components/organisms/MainContentsBottom"
+import WorldContentsInner from "../../../../components/organisms/WorldContentsInner"
+import PagerArea from "../../../../components/organisms/PagerArea"
+
+const WorldPage = () => {
+  return (
+    <>
+      <Helmet>
+        <html lang="ja" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>世界観・設定 | 「BLUE HUNTER」公式サイト</title>
+        <meta name="description" content="ブルーハンタープロジェクト公式サイト。2021年6月より漫画「BLUE HUNTER」を連載開始！最新ニュースや設定資料集、スペシャル情報等をこちらからお届けします。" />
+        <meta name="keywords" content="BLUE HUNTER, ブルーハンター, ブルーハンタープロジェクト, 無料漫画, アートコンペ " />
+        <link rel="canonical" href="https://manga.uminohi.jp/worldbuilding/machinery/sigma-glasses/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/worldbuilding/machinery/sigma-glasses/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="「BLUE HUNTER」公式サイト" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+        <link rel="preload" as="image" href="/common/nav_text_news_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_comic_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_world_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_characters_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_staff_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_special_hover.png" />
+      </Helmet>
+
+      {/* --------------------------------------- mal様修正範囲 開始 --------------------------------------- */}
+      <LayoutTwoColumn>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/world/ttl_world.png" alt="The World of BLUE HUNTER" />
+        </div>
+        {/* パンクズリスト */}
+        <Bread page01="TOP" href01="/" page02="世界観・設定" href02="../../index.html" page03="メカ・アイテム - -Σ(シグマ)グラス-" />
+        <WorldContentsInner>
+          <MainContentsTop text="メカ・アイテム" />
+          <MainContentsMiddleWorld>
+            <TextHeading text="-Σ(シグマ)グラス-" />
+            <ContentsLine />
+            <TextMedium text="ΣR（Σリアリティ）、ΣＶ（Σビュー）などに対応する多⽬的ゴーグル。ハンターギルドの創始者であるΣ（ミネルヴァや⽣ 頼などがメンバーとして在籍）が開発し、その後普及した。ハンターの眼を潮⾵や紫外線から守る意味もある。" />
+            <div className={styles.img_wrapper}>
+              <img src="/world/world_img_machinery_10.png" alt="" />
+            </div>
+            </MainContentsMiddleWorld>
+          <MainContentsBottom />
+          {/* PREV、NEXTボタンのURL、ページ番号の設定。1ページ目はPREVボタンを非活性、最終ページはNEXTボタンを非活性にします。 各ページごとに設定お願いします。*/}
+          <PagerArea hrefPrev="../sigma-jacket/" hrefNext="" isDisabledPrev={false} isDisabledNext={true}  number={9} />
+      {/* --------------------------------------- mal様修正範囲 終了 --------------------------------------- */}
+        </WorldContentsInner>
+      </LayoutTwoColumn>
+    </>
+  )
+}
+
+export default WorldPage
