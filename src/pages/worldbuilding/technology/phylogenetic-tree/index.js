cead577df1a22fc2c104a281db4439e7ad3c6ac7
emi kashizaki@otsukai <kashizaki@otsukai.com>
2021-06-01 15:59:56 +0900

    add: ワールド テクノロジーページ追加

---
+++ src/pages/worldbuilding/technology/phylogenetic-tree/index.js
@@ -0,0 +1,70 @@
+import React from "react"
+import * as styles from "./index.module.scss"
+import TextHeading from "../../../../components/atoms/TextHeading"
+import TextMedium from "../../../../components/atoms/TextMedium"
+import TextLeading from "../../../../components/atoms/TextLeading"
+import TextAnnotation from "../../../../components/atoms/TextAnnotation"
+import TextMediumSub from "../../../../components/atoms/TextMediumSub"
+import { Helmet } from "react-helmet"
+import LayoutTwoColumn from "../../../../components/templates/LayoutTwoColumn"
+import PageTitle from "../../../../components/atoms/PageTitle"
+import Bread from "../../../../components/molecules/Bread"
+import MainContentsTop from "../../../../components/organisms/MainContentsTop"
+import MainContentsMiddleWorld from "../../../../components/organisms/MainContentsMiddleWorld"
+import ContentsLine from "../../../../components/molecules/ContentsLine"
+import MainContentsBottom from "../../../../components/organisms/MainContentsBottom"
+import WorldContentsInner from "../../../../components/organisms/WorldContentsInner"
+import PagerArea from "../../../../components/organisms/PagerArea"
+import HeadLink from "../../../../components/templates/Headlink"
+
+const WorldPage = () => {
+  return (
+    <>
+      <Helmet>
+        <html lang="ja" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>世界観・設定 | 「BLUE HUNTER」公式サイト</title>
+        <meta name="description" content="ブルーハンタープロジェクト公式サイト。2021年6月より漫画「BLUE HUNTER」を連載開始！最新ニュースや設定資料集、スペシャル情報等をこちらからお届けします。" />
+        <meta name="keywords" content="BLUE HUNTER, ブルーハンター, ブルーハンタープロジェクト, 無料漫画, アートコンペ " />
+        <link rel="canonical" href="https://manga.uminohi.jp/worldbuilding/technology/phylogenetic-tree/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/worldbuilding/technology/phylogenetic-tree/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="「BLUE HUNTER」公式サイト" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+      </Helmet>
+      <HeadLink />
+
+      {/* --------------------------------------- mal様修正範囲 開始 --------------------------------------- */}
+      <LayoutTwoColumn>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/world/ttl_world.png" alt="The World of BLUE HUNTER" />
+        </div>
+        {/* パンクズリスト */}
+        <Bread page01="TOP" href01="/" page02="世界観・設定" href02="../../index.html" page03="テクノロジー - 系統樹" />
+        <WorldContentsInner>
+          <MainContentsTop text="テクノロジー" />
+          <MainContentsMiddleWorld>
+            <TextHeading text="系統樹" />
+            <ContentsLine />
+            <TextMedium text="⽣命の進化を「樹」に⾒⽴てて表したもので、⽣物の「分類」を「枝」に⾒⽴てている。すべての⽣物はある1つの種から⽣じ、その種がいろいろな「枝」に分かれ、さらにその枝がまた新たな枝に分かれ・・・といった具合にどんどん枝分かれしていき、最後には「葉(種にあたる)」がつく、といった具合。" />
+            <div className={styles.textMediumSub_wrapper}>
+              <TextMediumSub text="この系統樹という考え⽅は現在広く受け⼊れられているが、分⼦⽣物学の発展に伴い、実際はきれいに枝分かれがして
+おり葉がはっきりわかれている・・・というわけではなく、その葉同⼠も⼊り乱れている、という⾒⽅もある。描き⽅にも円型、あるいは始点のないものなど、様々ある。" />
+            </div>
+            <div className={styles.img_wrapper}>
+              <img src="/world/world_img_technology_02.png" alt="" />
+            </div>
+           </MainContentsMiddleWorld>
+          <MainContentsBottom />
+          {/* PREV、NEXTボタンのURL、ページ番号の設定。1ページ目はPREVボタンを非活性、最終ページはNEXTボタンを非活性にします。 各ページごとに設定お願いします。*/}
+          <PagerArea hrefPrev="../marine-metropolitan-areas/bioverse" hrefNext="" isDisabledPrev={false} isDisabledNext={true} number={4} />
+          {/* --------------------------------------- mal様修正範囲 終了 --------------------------------------- */}
+        </WorldContentsInner>
+      </LayoutTwoColumn>
+    </>
+  )
+}
+
+export default WorldPage
---
+++ src/pages/worldbuilding/technology/sigmar-sigmaview/index.js
@@ -0,0 +1,78 @@
+import React from "react"
+import * as styles from "./index.module.scss"
+import TextHeading from "../../../../components/atoms/TextHeading"
+import TextMedium from "../../../../components/atoms/TextMedium"
+import TextLeading from "../../../../components/atoms/TextLeading"
+import TextAnnotation from "../../../../components/atoms/TextAnnotation"
+import TextMediumSub from "../../../../components/atoms/TextMediumSub"
+import { Helmet } from "react-helmet"
+import LayoutTwoColumn from "../../../../components/templates/LayoutTwoColumn"
+import PageTitle from "../../../../components/atoms/PageTitle"
+import Bread from "../../../../components/molecules/Bread"
+import MainContentsTop from "../../../../components/organisms/MainContentsTop"
+import MainContentsMiddleWorld from "../../../../components/organisms/MainContentsMiddleWorld"
+import ContentsLine from "../../../../components/molecules/ContentsLine"
+import MainContentsBottom from "../../../../components/organisms/MainContentsBottom"
+import WorldContentsInner from "../../../../components/organisms/WorldContentsInner"
+import PagerArea from "../../../../components/organisms/PagerArea"
+import HeadLink from "../../../../components/templates/Headlink"
+
+const WorldPage = () => {
+  return (
+    <>
+      <Helmet>
+        <html lang="ja" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>世界観・設定 | 「BLUE HUNTER」公式サイト</title>
+        <meta name="description" content="ブルーハンタープロジェクト公式サイト。2021年6月より漫画「BLUE HUNTER」を連載開始！最新ニュースや設定資料集、スペシャル情報等をこちらからお届けします。" />
+        <meta name="keywords" content="BLUE HUNTER, ブルーハンター, ブルーハンタープロジェクト, 無料漫画, アートコンペ " />
+        <link rel="canonical" href="https://manga.uminohi.jp/worldbuilding/technology/sigmar-sigmaview/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/worldbuilding/technology/sigmar-sigmaview/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="「BLUE HUNTER」公式サイト" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+      </Helmet>
+      <HeadLink />
+
+      {/* --------------------------------------- mal様修正範囲 開始 --------------------------------------- */}
+      <LayoutTwoColumn>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/world/ttl_world.png" alt="The World of BLUE HUNTER" />
+        </div>
+        {/* パンクズリスト */}
+        <Bread page01="TOP" href01="/" page02="世界観・設定" href02="../../index.html" page03="テクノロジー - ΣＲ(シグマアール)/ΣV(シグマビュー)" />
+        <WorldContentsInner>
+          <MainContentsTop text="テクノロジー" />
+          <MainContentsMiddleWorld>
+            <TextHeading text="ΣＲ(シグマアール)/ΣV(シグマビュー)" />
+            <ContentsLine />
+            <TextMedium text="ハンターが海中で使う仮想空間U/I（およびそれを使うための⾼性能⽔中ゴーグル）。脳波コマンドに反応し、⼼の中で「海転！」と呟くことでログイン。ログアウトは「転海」。" />
+            <div className={styles.textMediumSub_wrapper}>
+              <TextMediumSub text="視界の中で海が真っ⽩になり、メッシュ状に区切られたグリッド（座標世界）が出現する。現実の海に1対1で対応しており、区切られた範囲を⽰す「ブロック」の⾊情報を頼りにハンターは移動し、考え、捕獲すべきターゲット⽣物を追い詰めていく。" />
+            </div>
+            <div className={styles.textMediumSub_wrapper}>
+              <TextMediumSub text="例）" />
+              <TextMediumSub text="⾚ブロック・・・環境DNAの痕跡" />
+              <TextMediumSub text="⻘ブロック・・・絞り込まれた予想エリア" />
+              <TextMediumSub text="⼗字・・・⽣物反応" />
+            </div>
+            <div className={styles.textMediumSub_wrapper}>
+              <TextMediumSub text="ハンターギルドの開祖「Σ」が開発したものである。そもそもΣには数学的に「積分」の意味もあり、ハンターに不可⽋な全情報を⼀⽬で認識できるような集約されたUIを⽬指し、開発された。" />
+            </div>
+            <div className={styles.img_wrapper}>
+              <img src="/world/world_img_01.png" alt="" />
+            </div>
+            </MainContentsMiddleWorld>
+          <MainContentsBottom />
+          {/* PREV、NEXTボタンのURL、ページ番号の設定。1ページ目はPREVボタンを非活性、最終ページはNEXTボタンを非活性にします。 各ページごとに設定お願いします。*/}
+          <PagerArea hrefPrev="" hrefNext="../ug/" isDisabledPrev={true} isDisabledNext={false} number={1} />
+          {/* --------------------------------------- mal様修正範囲 終了 --------------------------------------- */}
+        </WorldContentsInner>
+      </LayoutTwoColumn>
+    </>
+  )
+}
+
+export default WorldPage
