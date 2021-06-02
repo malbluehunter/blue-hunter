87d0b6a720ae0a088417a6d19479ee1fd6a92a10
emi kashizaki@otsukai <kashizaki@otsukai.com>
2021-06-02 16:41:07 +0900

    add: キャラクターページ

---
+++ src/pages/characters/hawk/index.js
@@ -0,0 +1,85 @@
+import React from "react"
+import * as styles from "./index.module.scss"
+import TextMedium from "../../../components/atoms/TextMedium"
+import { Helmet } from "react-helmet"
+import PageTitle from "../../../components/atoms/PageTitle"
+import BreadShort from "../../../components/molecules/BreadShort"
+import MainContentsMiddle from "../../../components/organisms/MainContentsMiddle"
+import ContentsInner from "../../../components/organisms/ContentsInner"
+import LayoutTwoColumn from "../../../components/templates/LayoutTwoColumn"
+import TextHeadingCharacters from "../../../components/atoms/TextHeadingCharacters"
+import Label02 from "../../../components/molecules/Label02"
+import SliderCharacters from "../../../components/organisms/SliderCharacters"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import BnrArt from "../../../components/atoms/BnrArt"
+import PagerAreaCharacters from "../../../components/organisms/PagerAreaCharacters"
+
+const CharactersPage = ({ location }) => {
+  return (
+    <>
+      <Helmet>
+        <html lang="ja" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>キャラクター | 「BLUE HUNTER」公式サイト</title>
+        <meta name="description" content="ブルーハンタープロジェクト公式サイト。2021年6月より漫画「BLUE HUNTER」を連載開始！最新ニュースや設定資料集、スペシャル情報等をこちらからお届けします。" />
+        <meta name="keywords" content="BLUE HUNTER, ブルーハンター, ブルーハンタープロジェクト, 無料漫画, アートコンペ " />
+        <link rel="canonical" href="https://manga.uminohi.jp/characters/hawk/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/characters/hawk/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="「BLUE HUNTER」公式サイト" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        <link rel="preload" as="image" href="/common/nav_text_news_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_comic_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_world_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_characters_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_staff_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_special_hover.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
+
+      {/* --------------------------------------- mal様修正範囲 開始 --------------------------------------- */}
+      <LayoutTwoColumn>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/characters/ttl_characters.png" alt="Characters" />
+        </div>
+        {/* パンクズリスト */}
+        <BreadShort page01="TOP" href01="/" page02="キャラクター - ホーク" />
+        <SliderCharacters pathname={location.pathname} />
+        <ContentsInner>
+          <MainContentsMiddle>
+            <div className={styles.contents_inner}>
+              <div className={styles.text_area}>
+                <TextHeadingCharacters text="ホーク" />
+                <div className={styles.lavel02_wrapper}>
+                  <Label02 text="誘導士/シーカー" />
+                </div>
+                <div className={styles.text_wrapper}>
+                  <TextMedium text="UNAVARAのライバルにあたる有⼒ギルド「セイヴィアン」に所属するでオペレイター（操縦⼠）を担う超⼀流のブル−ハンター。ガーデン3の主のような存在であり、⼤⼈・⼦供によらず信頼は厚い。腕っ節の強い17歳の少年で、UGの扱いにおいて天才肌。" />
+                  <TextMedium text="⽣来両⾜が無く、⾞椅⼦バスケットボールで鍛えたたくましい両腕で俊敏かつ繊細な動きをこなす。" />
+                  <TextMedium text="ガーデンではいつも⽉⼈と⾏動をともにしていた。⽉⼈の夢＝⾃分の夢であると感じている。⽉⼈を取られたと感じ、カケルとの対⽴を深めている。" />
+                </div>
+              </div>
+              <div className={styles.img_area}>
+                <img src="/characters/hawk.png" alt="ホーク" />
+              </div>
+            </div>
+          </MainContentsMiddle>
+          {/* --------------------------------------- mal様修正範囲 終了 --------------------------------------- */}
+        </ContentsInner>
+        {/* PREV、NEXTボタンのURL、ページ番号の設定。1ページ目はPREVボタンを非活性、最終ページはNEXTボタンを非活性にします。 各ページごとに設定お願いします。*/}
+        <PagerAreaCharacters hrefPrev="" hrefNext="./marine-metropolitan-areas/" isDisabledPrev={false} isDisabledNext={false} />
+        <div className={styles.bnr_area}>
+          <BnrArt />
+        </div>
+      </LayoutTwoColumn>
+    </>
+  )
+}
+
+export default CharactersPage
---
+++ src/pages/characters/nagisa/index.js
@@ -0,0 +1,83 @@
+import React from "react"
+import * as styles from "./index.module.scss"
+import TextMedium from "../../../components/atoms/TextMedium"
+import { Helmet } from "react-helmet"
+import PageTitle from "../../../components/atoms/PageTitle"
+import BreadShort from "../../../components/molecules/BreadShort"
+import MainContentsMiddle from "../../../components/organisms/MainContentsMiddle"
+import ContentsInner from "../../../components/organisms/ContentsInner"
+import LayoutTwoColumn from "../../../components/templates/LayoutTwoColumn"
+import TextHeadingCharacters from "../../../components/atoms/TextHeadingCharacters"
+import Label02 from "../../../components/molecules/Label02"
+import SliderCharacters from "../../../components/organisms/SliderCharacters"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import BnrArt from "../../../components/atoms/BnrArt"
+import PagerAreaCharacters from "../../../components/organisms/PagerAreaCharacters"
+
+const CharactersPage = ({ location }) => {
+  return (
+    <>
+      <Helmet>
+        <html lang="ja" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>キャラクター | 「BLUE HUNTER」公式サイト</title>
+        <meta name="description" content="ブルーハンタープロジェクト公式サイト。2021年6月より漫画「BLUE HUNTER」を連載開始！最新ニュースや設定資料集、スペシャル情報等をこちらからお届けします。" />
+        <meta name="keywords" content="BLUE HUNTER, ブルーハンター, ブルーハンタープロジェクト, 無料漫画, アートコンペ " />
+        <link rel="canonical" href="https://manga.uminohi.jp/characters/nagisa/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/characters/nagisa/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="「BLUE HUNTER」公式サイト" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        <link rel="preload" as="image" href="/common/nav_text_news_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_comic_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_world_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_characters_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_staff_hover.png" />
+        <link rel="preload" as="image" href="/common/nav_text_special_hover.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
+
+      {/* --------------------------------------- mal様修正範囲 開始 --------------------------------------- */}
+      <LayoutTwoColumn>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/characters/ttl_characters.png" alt="Characters" />
+        </div>
+        {/* パンクズリスト */}
+        <BreadShort page01="TOP" href01="/" page02="キャラクター - 上原渚" />
+        <SliderCharacters pathname={location.pathname} />
+        <ContentsInner>
+          <MainContentsMiddle>
+            <div className={styles.contents_inner}>
+              <div className={styles.text_area}>
+                <TextHeadingCharacters text="上原渚" />
+                <div className={styles.lavel02_wrapper}>
+                  <Label02 text="生物士" />
+                </div>
+                <div className={styles.text_wrapper}>
+                  <TextMedium text="海洋都市代表団にも名を連ねる世界屈指の研究者であり、ψ（プサイ）の頭脳。" />
+                </div>
+              </div>
+              <div className={styles.img_area}>
+                <img src="/characters/nagisa.png" alt="上原渚" />
+              </div>
+            </div>
+          </MainContentsMiddle>
+          {/* --------------------------------------- mal様修正範囲 終了 --------------------------------------- */}
+        </ContentsInner>
+        {/* PREV、NEXTボタンのURL、ページ番号の設定。1ページ目はPREVボタンを非活性、最終ページはNEXTボタンを非活性にします。 各ページごとに設定お願いします。*/}
+        <PagerAreaCharacters hrefPrev="" hrefNext="./marine-metropolitan-areas/" isDisabledPrev={false} isDisabledNext={false} />
+        <div className={styles.bnr_area}>
+          <BnrArt />
+        </div>
+      </LayoutTwoColumn>
+    </>
+  )
+}
+
+export default CharactersPage
