3ac15bf5d048cafe44fc46201917a4589f64c768
emi kashizaki <kashizaki@otsukai.com>
2021-06-11 14:15:39 +0900

    add: コミック英語ページ追加

---
+++ src/pages/en/comic/ep1/index.js
@@ -0,0 +1,77 @@
+import React from "react"
+import PageTitle from "../../../../components/atoms/PageTitle"
+import * as styles from "./index.module.scss"
+import { Helmet } from "react-helmet"
+import BreadShort from "../../../../components/molecules/BreadShort"
+import ComicArea from "../../../../components/organisms/ComicArea"
+import ShareArea from "../../../../components/molecules/ShareArea"
+import LayoutTwoColumnComic from "../../../../components/templates/LayoutTwoColumnComic"
+import ComicContentsInner from "../../../../components/organisms/ComicContentsInner"
+import Button from "../../../../components/atoms/Button"
+import Label01 from "../../../../components/molecules/Label01"
+
+const ComicPage = ({ location }) => {
+  // /* --------------------------------------- mal様修正範囲(マンガ画像を表示順で設定) 開始 --------------------------------------- */
+  const src = ["/comic/img_comic_01.jpg", "/comic/img_comic_02.jpg"]
+  // /* --------------------------------------- mal様修正範囲(マンガ画像を表示順で設定) 終了 --------------------------------------- */
+
+  return (
+    <>
+      <Helmet>
+        <html lang="en" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>Episode 1 | BLUE HUNTER Official Site</title>
+        <meta name="description" content="The official website for the BLUE HUNTER project. Free BLUE HUNTER manga starts June 2021! Check out all you need to know about the world of BLUE HUNTER here." />
+        <meta name="keywords" content="BLUE HUNTER, BH, BLUE HUNTER Project, Free Comic, Art Contest  " />
+        <link rel="canonical" href="https://manga.uminohi.jp/en/comic/ep1/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/en/comic/ep1/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="BLUE HUNTER Official Site" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+        <link rel="preload" as="image" href="/common/nav_text_news_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_comic_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_world_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_characters_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_staff_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_special_hover.webp" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
+
+      {/* --------------------------------------- mal様修正範囲 開始 --------------------------------------- */}
+      <LayoutTwoColumnComic pathname={location.pathname}>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/comic/ttl_comic.png" />
+        </div>
+        {/* パンクズリスト */}
+        <div className={styles.bread_wrapper}>
+          <BreadShort page01="TOP" href01="/" page02="Comic - Episode 1" />
+        </div>
+        <Label01 text="【全13話】毎週x曜日最新話公開！" />
+        <ComicContentsInner>
+          <div className={styles.comic_wrapper}>
+            <ComicArea src={src} />
+          </div>
+          <div>
+            <ShareArea />
+            <div className={styles.btn_wrapper}>
+              <div className={styles.btn_next}>
+                <Button href="#" isDisabled={false}>
+                  Read Chapter X
+                </Button>
+              </div>
+              <div className={styles.btn_plan}>
+                <Button isDisabled={true}>Next chapter out X Aug</Button>
+              </div>
+            </div>
+          </div>
+        </ComicContentsInner>
+      </LayoutTwoColumnComic>
+      {/* --------------------------------------- mal様修正範囲 終了 --------------------------------------- */}
+    </>
+  )
+}
+
+export default ComicPage
