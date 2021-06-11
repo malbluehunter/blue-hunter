d27a3953d04736929750020f75a6d6108f17c012
emi kashizaki <kashizaki@otsukai.com>
2021-06-11 17:16:36 +0900

    add: 英語スペシャルページ追加（未翻訳あり）

---
+++ src/pages/en/special/index.js
@@ -0,0 +1,228 @@
+import React from "react"
+import * as styles from "./index.module.scss"
+import { Helmet } from "react-helmet"
+import PageTitle from "../../../components/atoms/PageTitle"
+import BreadShort from "../../../components/molecules/BreadShort"
+import MainContentsMiddle from "../../../components/organisms/MainContentsMiddle"
+import ContentsInner from "../../../components/organisms/ContentsInner"
+import LayoutTwoColumn from "../../../components/templates/LayoutTwoColumn"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import TextHeading from "../../../components/atoms/TextHeading"
+import TextMedium from "../../../components/atoms/TextMedium"
+import LineImg from "../../../components/atoms/LineImg"
+import LineImgSp from "../../../components/atoms/LineImgSp"
+import Label05 from "../../../components/molecules/Label05"
+import Label04 from "../../../components/molecules/Label04"
+import TextLarge from "../../../components/atoms/TextLarge"
+
+const SpecialPage = ({ location }) => {
+  return (
+    <>
+      <Helmet>
+        <html lang="en" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>Special | BLUE HUNTER Official Site</title>
+        <meta name="description" content="The official website for the BLUE HUNTER project. Free BLUE HUNTER manga starts June 2021! Check out all you need to know about the world of BLUE HUNTER here." />
+        <meta name="keywords" content="BLUE HUNTER, BH, BLUE HUNTER Project, Free Comic, Art Contest " />
+        <link rel="canonical" href="https://manga.uminohi.jp/en/special/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/en/special/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="BLUE HUNTER Official Site" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        <link rel="preload" as="image" href="/common/nav_text_news_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_comic_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_world_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_characters_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_staff_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_special_hover.webp" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
+
+      {/* --------------------------------------- mal様修正範囲 開始 --------------------------------------- */}
+      <LayoutTwoColumn pathname={location.pathname}>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/special/ttl_special.png" alt="Special" />
+        </div>
+        {/* パンクズリスト */}
+        <BreadShort page01="TOP" href01="/" page02="Special" />
+        <ContentsInner>
+          <MainContentsMiddle>
+            <div className={styles.contents_inner}>
+              <div className={styles.section01}>
+                <div className={styles.textHeading_wrapper}>
+                  <TextHeading text="最終選考審査会(仮）" />
+                </div>
+                <div className={styles.text_wrapper}>
+                  <TextMedium text="表彰イベントの当日の様子を公開しています。などの簡単な説明文が入ります。表彰イベントの当日の様子を公開しています。などの簡単な説明文が入ります。" />
+                </div>
+                <div className={styles.video}>
+                  <iframe
+                    width="100%"
+                    height="315"
+                    src="https://www.youtube.com/embed/0jqxkDfmvYo"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+              </div>
+              <div className={styles.section02}>
+                <div className={`${styles.textHeading_wrapper} ${styles.view_pc}`}>
+                  <TextHeading text={"アートコンペ受賞者結果発表"} />
+                </div>
+                <div className={`${styles.textHeading_wrapper} ${styles.view_sp}`}>
+                  <TextHeading text={"アートコンペ受賞者\n結果発表"} />
+                </div>
+                <div className={styles.line_wrapper}>
+                  <LineImg pathname={location.pathname} />
+                  <LineImgSp pathname={location.pathname} />
+                </div>
+                <Label04 text="Blue Hunter 大賞" />
+                <div className={styles.prizeMoney_wrapper}>
+                  <p>賞金 50万円</p>
+                </div>
+                <div className={styles.img_wrapper}>
+                  <img src="/special/special_img01.jpg" alt="" />
+                </div>
+                <div className={styles.prizeWinner_wrapper}>
+                  <div className={styles.prizeWinner_item}>
+                    <Label05 text="作品名" />
+                    <div className={styles.prizeWinner_item_text}>
+                      <TextLarge text="作品タイトルが入ります" />
+                    </div>
+                  </div>
+                  <div className={styles.prizeWinner_item}>
+                    <Label05 text="受賞者" />
+                    <div className={styles.prizeWinner_item_text}>
+                      <TextLarge text="山田 太郎" />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.text_wrapper}>
+                  <TextMedium
+                    text={
+                      "受賞者による作品の説明が入ります。（ナラティブです。）\n私は今日初めてこの学習院というものの中に這入はいりました。もっとも以前から学習院は多分この見当だろうぐらいに考えていたには相違そういありませんが、はっきりとは存じませんでした。中へ這入ったのは無論今日が初めてでございます。"
+                    }
+                  />
+                </div>
+              </div>
+              <div className={styles.section03}>
+                <div className={styles.line_wrapper}>
+                  <LineImg pathname={location.pathname} />
+                  <LineImgSp pathname={location.pathname} />
+                </div>
+                <Label04 text="審査員特別賞" />
+                <div className={styles.prizeMoney_wrapper}>
+                  <p>賞金 10万円</p>
+                </div>
+                <div className={styles.img_wrapper}>
+                  <img src="/special/special_img01.jpg" alt="" />
+                </div>
+                <div className={styles.prizeWinner_wrapper}>
+                  <div className={styles.prizeWinner_item}>
+                    <Label05 text="作品名" />
+                    <div className={styles.prizeWinner_item_text}>
+                      <TextLarge text="作品タイトルが入ります" />
+                    </div>
+                  </div>
+                  <div className={styles.prizeWinner_item}>
+                    <Label05 text="受賞者" />
+                    <div className={styles.prizeWinner_item_text}>
+                      <TextLarge text="山田 太郎" />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.text_wrapper}>
+                  <TextMedium
+                    text={
+                      "受賞者による作品の説明が入ります。（ナラティブです。）\n私は今日初めてこの学習院というものの中に這入はいりました。もっとも以前から学習院は多分この見当だろうぐらいに考えていたには相違そういありませんが、はっきりとは存じませんでした。中へ這入ったのは無論今日が初めてでございます。"
+                    }
+                  />
+                </div>
+              </div>
+              <div className={styles.dot_wrapper}>
+                <img src="/special/dot_img.png" alt="・・・" />
+              </div>
+              <div className={styles.section02}>
+                <div className={styles.textHeading_wrapper}>
+                  <TextHeading text="アートコンペ概要" />
+                </div>
+                <div className={styles.line_wrapper}>
+                  <LineImg pathname={location.pathname} />
+                  <LineImgSp pathname={location.pathname} />
+                </div>
+                {/* テーブル(見出しなし) 開始 */}
+                <div className={styles.table_sub_wrapper}>
+                  <table className={styles.table_sub}>
+                    <tr className={styles.table_tr}>
+                      <td className={styles.td_leading}>テーマ</td>
+                      <td className={styles.td_item}>
+                        ブルーハンターの世界を4つのテーマに分けて、アート作品を募集。
+                        <br />
+                        （１）メカ部門・・・ハンターたちが使いそうなアイテムや乗り物
+                        <br />
+                        （２）生物部門・・・ブルーハンターの世界に存在しそうな生き物
+                        <br />
+                        （３）ランドスケープ部門・・・温暖化や海面上昇が進んだ都市風景、温暖化が解消された建造物やインフラなど
+                        <br />
+                        （４）ブルーハンター部門・・・あなたが考える新キャラクターのデザイン
+                      </td>
+                    </tr>
+                    <tr className={styles.table_tr}>
+                      <td className={styles.td_leading}>賞</td>
+                      <td className={styles.td_item}>
+                        ・BH大賞　賞金50万円
+                        <br />
+                        ・審査員特別賞　賞金10万円
+                        <br />
+                        ・ナラティブ賞　賞金10万円
+                        <br />
+                        ・MALユーザー賞　賞金5万円
+                        <br />
+                        ・メカ部門賞　賞金5万円
+                        <br />
+                        ・生物部門賞　賞金5万円
+                        <br />
+                        ・ランドスケープ部門賞　賞金5万円
+                        <br />
+                        ・ブルーハンター部門賞　賞金5万円
+                        <br />
+                        ・キッズ部門賞　賞金5万円
+                      </td>
+                    </tr>
+                    <tr className={styles.table_tr}>
+                      <td className={styles.td_leading}>応募期間</td>
+                      <td className={styles.td_item}>2020年6月7日（月）〜8月30日（月）</td>
+                    </tr>
+                    <tr className={styles.table_tr}>
+                      <td className={styles.td_leading}>Web投票期間</td>
+                      <td className={styles.td_item}>2020年8月x日（x）〜x月x日（x）</td>
+                    </tr>
+                    <tr className={styles.table_tr}>
+                      <td className={styles.td_leading}>応募総数</td>
+                      <td className={styles.td_item}>1000件</td>
+                    </tr>
+                  </table>
+                </div>
+                {/* テーブル(見出しなし) 終了 */}
+                <div className={styles.text_wrapper}>
+                  <TextMedium text={"受賞者におかれましては、おめでとうございます。また、今回アートコンペに参加していただいたすべての皆さまに、心より感謝申し上げます。"} />
+                </div>
+              </div>
+            </div>
+          </MainContentsMiddle>
+          {/* --------------------------------------- mal様修正範囲 終了 --------------------------------------- */}
+        </ContentsInner>
+      </LayoutTwoColumn>
+    </>
+  )
+}
+
+export default SpecialPage
---
+++ src/pages/en/special/index02.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from "react"
+import * as styles from "./index02.module.scss"
+import { Helmet } from "react-helmet"
+import PageTitle from "../../../components/atoms/PageTitle"
+import BreadShort from "../../../components/molecules/BreadShort"
+import MainContentsMiddle from "../../../components/organisms/MainContentsMiddle"
+import ContentsInner from "../../../components/organisms/ContentsInner"
+import LayoutTwoColumn from "../../../components/templates/LayoutTwoColumn"
+import TextHeading from "../../../components/atoms/TextHeading"
+import TextMedium from "../../../components/atoms/TextMedium"
+import LineImg from "../../../components/atoms/LineImg"
+import LineImgSp from "../../../components/atoms/LineImgSp"
+import BnrArt from "../../../components/atoms/BnrArt"
+import TextAnnotation from "../../../components/atoms/TextAnnotation"
+import ButtonLiquid from "../../../components/atoms/ButtonLiquid"
+
+const SpecialPage = ({ location }) => {
+  const [src, setSrc] = useState([])
+
+  useEffect(() => {
+    fetch(
+      "https://graph.facebook.com/17843900656018477/recent_media?user_id=17841447571286718&fields=id,media_url,media_type,permalink,children{id,media_type,media_url,permalink}&access_token=EAAGhQ8fkHakBAPFpXN8lCxC00bAa4KVtdpN8RbSEGNz1TfL6kXXOzkWqfZBoBZBMOf6XhdZBOWVReoG3u9pQqEZC2HXAibWBpdXNZCy9vFSpLD0IO2Ie0MxjQosRzZAzLhiuQi3yZBM8evEq0Xz5z74PIsZBPz6OmqBfspFMmvSWqtxottj4heBk"
+    ).then(response => {
+      response
+        .json() //ここでBodyからJSONを返す
+        .then(result => {
+          setSrc(result.data)
+        })
+        .catch(e => {
+          console.log(e) //エラーをキャッチし表示
+        })
+    })
+  }, [])
+
+  return (
+    <>
+      <Helmet>
+        <html lang="en" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 開始 --------------------------------------- */}
+        <title>Special | BLUE HUNTER Official Site</title>
+        <meta name="description" content="The official website for the BLUE HUNTER project. Free BLUE HUNTER manga starts June 2021! Check out all you need to know about the world of BLUE HUNTER here." />
+        <meta name="keywords" content="BLUE HUNTER, BH, BLUE HUNTER Project, Free Comic, Art Contest " />
+        <link rel="canonical" href="https://manga.uminohi.jp/en/special/" />
+        <meta property="og:url" content="https://manga.uminohi.jp/en/special/" />
+        {/* --------------------------------------- mal様修正範囲(meta情報) 終了 --------------------------------------- */}
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="BLUE HUNTER Official Site" />
+        <meta property="og:image" content="https://manga.uminohi.jp/ogp.png" />
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        <link rel="preload" as="image" href="/common/nav_text_news_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_comic_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_world_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_characters_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_staff_hover.webp" />
+        <link rel="preload" as="image" href="/common/nav_text_special_hover.webp" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
+
+      <LayoutTwoColumn pathname={location.pathname}>
+        {/* ページタイトル */}
+        <div className={styles.pageTitle_wrapper}>
+          <PageTitle src="/special/ttl_special.png" alt="Special" />
+        </div>
+        {/* パンクズリスト */}
+        <BreadShort page01="TOP" href01="/" page02="Special" />
+        <div className={styles.contents_01}>
+          <ContentsInner>
+            <MainContentsMiddle>
+              <div className={styles.contents_inner}>
+                <div className={styles.textHeading_wrapper}>
+                  <TextHeading text={"Take part in a contest for Blue Hunter-related art!"} />
+                </div>
+                <div className={styles.line_wrapper}>
+                  <LineImg pathname={location.pathname} />
+                  <LineImgSp pathname={location.pathname} />
+                </div>
+                <div className={styles.text_wrapper}>
+                  <TextMedium text="ブルーハンターをテーマにしたアートコンペを開催中です。詳しくは以下をご覧ください。" />
+                </div>
+                <div className={styles.bnr_wrapper}>
+                  <BnrArt />
+                </div>
+              </div>
+            </MainContentsMiddle>
+          </ContentsInner>
+        </div>
+        <div className={styles.contents_02}>
+          <ContentsInner>
+            <MainContentsMiddle>
+              <div className={styles.contents_inner}>
+                <TextHeading text="Contest Entries" />
+                <div className={styles.leading_wrapper}>
+                  <TextAnnotation text="See entries in order of popularity" />
+                </div>
+              </div>
+            </MainContentsMiddle>
+          </ContentsInner>
+        </div>
+        <div className={styles.insta_area}>
+          <div className={styles.insta_inner}>
+            {src.map((srcItem, index) =>
+              srcItem.media_type == "CAROUSEL_ALBUM" ? ( // 画像が複数の場合
+                srcItem.children.data[0].media_type == "IMAGE" ? ( // 複数画像かつ画像がイメージの場合
+                  <a href={srcItem.children.data[0].permalink}>
+                    <img src={srcItem.children.data[0].media_url} key={index} className={styles.insta_img} loading="lazy" />
+                  </a>
+                ) : (
+                  // 複数画像かつ画像が動画の場合
+                  <a href={srcItem.children.data[0].permalink}>
+                    <video src={srcItem.children.data[0].media_url} key={index} className={styles.insta_img} loading="lazy" />
+                  </a>
+                )
+              ) : srcItem.media_type == "IMAGE" ? ( // 画像が1枚の場合
+                <a href={srcItem.permalink}>
+                  <img src={srcItem.media_url} key={index} className={styles.insta_img} loading="lazy" />
+                </a>
+              ) : (
+                // 画像が1枚かつ動画の場合
+                <a href={srcItem.permalink}>
+                  <video src={srcItem.media_url} key={index} className={styles.insta_img} loading="lazy" />
+                </a>
+              )
+            )}
+          </div>
+        </div>
+        <div className={styles.btn_wrapper}>
+          <ButtonLiquid href="#" isDisabled={false}>
+            See more with the #bh_mal tag
+            <div className={styles.icon_wrapper}>
+              <img src="/common/icon_instagram_02.svg" alt="インスタグラム アイコン" className={styles.icon} />
+            </div>
+          </ButtonLiquid>
+        </div>
+      </LayoutTwoColumn>
+    </>
+  )
+}
+
+export default SpecialPage
