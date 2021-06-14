95f5552463ef9beec3449a5ce8f6749d66cf0aad
kanou.s <kanou-s@tam-tam.co.jp>
2021-06-14 10:51:39 +0900

    fix:アクセストークン

--- src/pages/special/index02.js
+++ src/pages/special/index02.js
@@ -16,7 +16,7 @@ import ButtonLiquid from "../../components/atoms/ButtonLiquid"
 
 const SpecialPage = ({ location }) => {
   const [src, setSrc] = useState([])
-  const [src02, setSrc02] = useState([]) //26件以降の画像
+  // const [src02, setSrc02] = useState([]) //26件以降の画像
 
   useEffect(() => {
     // 1~25件のデータを取得
@@ -32,19 +32,19 @@ const SpecialPage = ({ location }) => {
         })
     })
     // 26~40件のデータを取得
-    fetch(
-      "https://graph.facebook.com/v11.0/17843900656018477/recent_media?access_token=EAAGhQ8fkHakBAAv7gYN0fg2Tfv3QlTH1sLIaXAZBaBantKfW42yeQidrYH90G2fHsvDqYRZBZCsxycdycRZCTD7LF5yGgZBHwsI4Dn1kQpAJxHx9GpK105ai6zN53ZAZCcNDQ83vs8z8pfe4rMzEM0LQpn7rKn0slnS2wNeZArXhPV9MJ2LxDwqn&pretty=1&fields=id%2Cmedia_url%2Cmedia_type%2Cpermalink%2Cchildren%7Bid%2Cmedia_type%2Cmedia_url%2Cpermalink%7D&user_id=17841447571286718&limit=25&after=UVZAGRVkxTldYMVpNTWxnMlV6SkxlVU5YYWtaUlYwUjVNMmhtV0RKU2RtSTNUVXBHT1ZAGTWFpMUtZAMUZAUWVU1Nk1pMVlkRnBRYmxkdGFFaE1UVFUyZAGpCV1MzSndRamt0WTBsQ1pEVmhUbGh6VTA0M1QxaEVSQT09"
-    ).then(response => {
-      response
-        .json() //ここでbodyからJSONを返す
-        .then(result => {
-          setSrc02(result.data)
-          console.log(result.data)
-        })
-        .catch(e => {
-          console.log(e)
-        })
-    })
+    // fetch(
+    //   "https://graph.facebook.com/v11.0/17843900656018477/recent_media?access_token=EAAGhQ8fkHakBAAv7gYN0fg2Tfv3QlTH1sLIaXAZBaBantKfW42yeQidrYH90G2fHsvDqYRZBZCsxycdycRZCTD7LF5yGgZBHwsI4Dn1kQpAJxHx9GpK105ai6zN53ZAZCcNDQ83vs8z8pfe4rMzEM0LQpn7rKn0slnS2wNeZArXhPV9MJ2LxDwqn&pretty=1&fields=id%2Cmedia_url%2Cmedia_type%2Cpermalink%2Cchildren%7Bid%2Cmedia_type%2Cmedia_url%2Cpermalink%7D&user_id=17841447571286718&limit=25&after=UVZAGRVkxTldYMVpNTWxnMlV6SkxlVU5YYWtaUlYwUjVNMmhtV0RKU2RtSTNUVXBHT1ZAGTWFpMUtZAMUZAUWVU1Nk1pMVlkRnBRYmxkdGFFaE1UVFUyZAGpCV1MzSndRamt0WTBsQ1pEVmhUbGh6VTA0M1QxaEVSQT09"
+    // ).then(response => {
+    //   response
+    //     .json() //ここでbodyからJSONを返す
+    //     .then(result => {
+    //       setSrc02(result.data)
+    //       console.log(result.data)
+    //     })
+    //     .catch(e => {
+    //       console.log(e)
+    //     })
+    // })
   }, [])
 
   return (
