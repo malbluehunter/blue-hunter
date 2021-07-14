fbb6ca68954bf88534b1cba3406f268b64963cb5
kanou.s <kanou-s@tam-tam.co.jp>
2021-07-14 13:05:29 +0900

    fix:short name 修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        short_name: `BLUE HUNTER`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
