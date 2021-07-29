b21b979cfb6a505c88ee03951dd074a5f64853e2
emi kashizaki <kashizaki@otsukai.com>
2021-07-29 15:19:21 +0900

    fix: テキスト修正

--- src/pages/worldbuilding/machinery/sigma-glasses/index.js
+++ src/pages/worldbuilding/machinery/sigma-glasses/index.js
@@ -53,7 +53,7 @@ const WorldPage = ({ location }) => {
           <MainContentsMiddleWorld pathname={location.pathname}>
             <TextHeading text="-Σ(シグマ)グラス-" />
             <ContentsLine pathname={location.pathname} />
-            <TextMedium text="ΣR（Σリアリティ）、ΣＶ（Σビュー）などに対応する多⽬的ゴーグル。ハンターギルドの創始者であるΣ（シグマ）が開発し、その後普及した。ハンターの眼を潮⾵や紫外線から守る意味もある。" />
+            <TextMedium text="ΣR（Σアール）、ΣＶ（Σビュー）などに対応する多⽬的ゴーグル。ハンターギルドの創始者であるΣ（シグマ）が開発し、その後普及した。ハンターの眼を潮⾵や紫外線から守る意味もある。" />
             <div className={styles.img_wrapper}>
               <img src="/world/world_img_machinery_10.png" alt="" />
             </div>
