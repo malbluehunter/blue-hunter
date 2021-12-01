8ba63aa9a3444857bc2aeb2469e566070e53f4f9
emi kashizaki <kashizaki@otsukai.com>
2021-12-01 14:09:31 +0900

    fix: 日付修正

--- src/components/templates/LocalNavNews.js
+++ src/components/templates/LocalNavNews.js
@@ -12,7 +12,7 @@ const LocalNavNews = () => {
         <ol className={styles.localNav_list} start="0">
         <li className={`${styles.localNav_item} ${styles.current}`}>
             <Link to="#news_11">第10話を公開しました</Link>
-            <p className={styles.date}>2021.11.1</p>
+            <p className={styles.date}>2021.12.3</p>
           </li>
           <li className={`${styles.localNav_item}`}>
             <Link to="#news_10">第9話を公開しました</Link>
