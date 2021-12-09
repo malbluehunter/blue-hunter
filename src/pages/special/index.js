308a6ba28af4c85c1465c495e3bd3446e6c6df6a
emi kashizaki <kashizaki@otsukai.com>
2021-12-09 09:12:51 +0900

    fix: 表彰式動画 youtube URL差し替え

--- src/pages/special/index.js
+++ src/pages/special/index.js
@@ -62,7 +62,7 @@ const SpecialPage = ({ location }) => {
                   <iframe
                     width="100%"
                     height="315"
-                    src="https://www.youtube.com/embed/0jqxkDfmvYo"
+                    src="https://www.youtube.com/embed/0YEmUpLxjyk"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
